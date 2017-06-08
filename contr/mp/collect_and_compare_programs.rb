c4258dacbd3773b174c757e8a2fc83c3542590a1
MIachaI <mprawda@wp.pl>
2017-06-09 00:38:30 +0200

    variables with proper names, last loop to iterate throught arrays elements

--- contr/mp/collect_and_compare_programs.rb
+++ contr/mp/collect_and_compare_programs.rb
@@ -9,33 +9,38 @@ the data inside. I will implement comparision algorythm later.
   FileLengthTab = []
   FileHeightTab = []
   programLength=0;
-  i=0;
+  programHeigth=0;
 
   Dir.foreach("testFolder") {
 
-      |x|
-    if (x.length<4)
+      |currentyFile|
+    if (currentyFile.length<4)
       next;
 
     end
-    puts "Opening: #{x}..."
+    puts "Opening: #{currentyFile}..."
     puts "Extracting..."
-    file = File.new("testFolder/#{x}", "r")
+    file = File.new("testFolder/#{currentyFile}", "r")
     while (line = file.gets)
       puts "#{line}"
       programLength += line.length
-      i=i+1
+      programHeigth=programHeigth+1
     end
     file.close
-    FileHeightTab.push(i)
-    FileNameTab.push(x)
+    FileHeightTab.push(programHeigth)
+    FileNameTab.push(currentyFile)
     FileLengthTab.push(programLength)
 
     programLength=0;
-    i=0;
+    programHeigth=0;
   }
-  puts "\nExecution successful."
-  puts FileNameTab
-  puts FileLengthTab
-  puts FileHeightTab
+  while(FileNameTab.size!=0)
+    puts "\nExecution successful."
+    printf("File name: ")
+    puts(FileNameTab.pop)
+    printf("File length: ")
+    puts(FileLengthTab.pop)
+    printf("File height: ")
+    puts(FileHeightTab.pop)
   end
+end

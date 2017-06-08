e5268501066d1bd026d18c9e9462bd9037d4c7bd
MIachaI <mprawda@wp.pl>
2017-06-08 22:58:14 +0200

    Initial commit

---
+++ collect_and_compare_programs.rb
@@ -0,0 +1,22 @@
+class CollectAndComparePrograms
+=begin
+This is basic concept of my part for the SRiR project. It requires just signal
+from the main part of the program to start searching for stored files.
+Once it found any, it loops for each file in directory and extracting
+the data inside. I will implement comparision algorythm later.
+=end
+  Dir.foreach("testFolder") {
+      |x|
+    if (x.length<4)
+      next;
+    end
+    puts "Opening: #{x}..."
+    puts "Extracting..."
+    file = File.new("testFolder/#{x}", "r")
+    while (line = file.gets)
+      puts "#{line}"
+    end
+    file.close
+  }
+  puts "\nExecution successful."
+  end

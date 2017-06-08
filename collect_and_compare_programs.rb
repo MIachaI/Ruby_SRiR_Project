5a43bb4d702865cf91243bb7983c45375c212465
MIachaI <mprawda@wp.pl>
2017-06-09 00:12:48 +0200

    This compares files by names, length and number of lines

--- collect_and_compare_programs.rb
+++ collect_and_compare_programs.rb
@@ -5,18 +5,37 @@ from the main part of the program to start searching for stored files.
 Once it found any, it loops for each file in directory and extracting
 the data inside. I will implement comparision algorythm later.
 =end
+  FileNameTab = []
+  FileLengthTab = []
+  FileHeightTab = []
+  programLength=0;
+  i=0;
+
   Dir.foreach("testFolder") {
+
       |x|
     if (x.length<4)
       next;
+
     end
     puts "Opening: #{x}..."
     puts "Extracting..."
     file = File.new("testFolder/#{x}", "r")
     while (line = file.gets)
       puts "#{line}"
+      programLength += line.length
+      i=i+1
     end
     file.close
+    FileHeightTab.push(i)
+    FileNameTab.push(x)
+    FileLengthTab.push(programLength)
+
+    programLength=0;
+    i=0;
   }
   puts "\nExecution successful."
+  puts FileNameTab
+  puts FileLengthTab
+  puts FileHeightTab
   end

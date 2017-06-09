6c8ece3e5ebd831754fee8f68ed0edb08d0079bb
MIachaI <mprawda@wp.pl>
2017-06-09 16:38:46 +0200

    minor changes, method "compareFiles" added and invoked, output changed

--- contr/mp/collect_and_compare_programs.rb
+++ contr/mp/collect_and_compare_programs.rb
@@ -5,42 +5,66 @@ from the main part of the program to start searching for stored files.
 Once it found any, it loops for each file in directory and extracting
 the data inside. I will implement comparision algorythm later.
 =end
+
   FileNameTab = []
   FileLengthTab = []
   FileHeightTab = []
+  FileComplexityTab =[]
+# Compare all files in selected input directory
+# @Return Report.txt as summary
+public def compareFiles()
   programLength=0;
   programHeigth=0;
-
+  programComplexity=0;
   Dir.foreach("testFolder") {
 
-      |currentyFile|
-    if (currentyFile.length<4)
+      |currentFile|
+    if (currentFile.length<4)
       next;
 
     end
-    puts "Opening: #{currentyFile}..."
+    puts "Opening: #{currentFile}"
     puts "Extracting..."
-    file = File.new("testFolder/#{currentyFile}", "r")
+    file = File.new("testFolder/#{currentFile}", "r")
     while (line = file.gets)
-      puts "#{line}"
+
+      if line.include? "end"
+        programComplexity+=1
+      end
       programLength += line.length
       programHeigth=programHeigth+1
     end
+    puts "Closing file #{currentFile}\n\n"
+
     file.close
+    FileComplexityTab.push(programComplexity)
     FileHeightTab.push(programHeigth)
-    FileNameTab.push(currentyFile)
+    FileNameTab.push(currentFile)
     FileLengthTab.push(programLength)
 
     programLength=0;
     programHeigth=0;
+    programComplexity=0;
   }
-  while(FileNameTab.size!=0)
-    puts "\nExecution successful."
-    printf("File name: ")
-    puts(FileNameTab.pop)
-    printf("File length: ")
-    puts(FileLengthTab.pop)
-    printf("File height: ")
-    puts(FileHeightTab.pop)
+
+  open('report.txt', 'w') do |f|
+    f.printf("Length - how many characters are in file\n")
+    f.printf("Height - how many lines this file contains\n")
+    f.printf("Complexity - how many methods this file use\n\n")
+    while (FileNameTab.size!=0)
+    f.printf("File name: ")
+    f.puts(FileNameTab.pop)
+    f.printf("File length: ")
+    f.puts(FileLengthTab.pop)
+    f.printf("Height: ")
+    f.puts(FileHeightTab.pop)
+    f.printf("Complexity: ")
+    f.puts(FileComplexityTab.pop)
+    f.puts("\n\n")
+   end
   end
 end
+end
+
+testObject = CollectAndComparePrograms.new()
+testObject.compareFiles

49f1593c683047a18bec06e3a3596804e0cbabc0
umatbro <umatbroo@gmail.com>
2017-06-13 23:33:07 +0200

    kazdy polaczony klient do nowego watku

--- contr/mu/server.rb
+++ contr/mu/server.rb
@@ -10,22 +10,23 @@ class Server
   def run
     server = TCPServer.open(@port)
     loop {
-      client = server.accept
-      # odbiera tylko 1000 znakow (do poprawy)
-      codex = client.recv(1000)
-      code = Code.new(codex)
+      Thread.start(server.accept) do |client|
+        # odbiera tylko 1000 znakow (do poprawy)
+        codex = client.recv(1000)
+        code = Code.new(codex)
 
-      @server_response = code.check_syntax
-      if @server_response
-        @server_response = "\rKod poprawny"
-        @server_response += "\nOutput programu: \n"
-        @server_response += code.code_output
-      else
-        @server_response = "\nBłąd. Opis: \n"
-        @server_response += code.syntax_error_msg
+        @server_response = code.check_syntax
+        if @server_response
+          @server_response = "\rKod poprawny"
+          @server_response += "\nOutput programu: \n"
+          @server_response += code.code_output
+        else
+          @server_response = "\nBłąd. Opis: \n"
+          @server_response += code.syntax_error_msg
+        end
+        client.print(@server_response)
+        client.close
       end
-      client.print(@server_response)
-      client.close
     }
   end
 end

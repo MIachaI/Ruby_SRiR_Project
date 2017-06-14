b8c98e6e1797f440846e6cea98e78936b62d8aba
umatbro <umatbroo@gmail.com>
2017-06-14 14:39:45 +0200

    przesłane kody zapisywane do nowych plikow

--- contr/mu/main.rb
+++ contr/mu/main.rb
@@ -1,11 +1,6 @@
 require_relative "code_tester"
 require_relative "server"
 
-# przykład odczytania kodu i wykonania go z argumentami
-# code = Code.new(File.read "test_file.rb")
-# puts code.code_output [1,3]
-# code.save_code_to_file("teset.rb")
-
 port = 1999
 server = Server.new(port)
 puts "Zaczynam nowy serwer na porcie #{port}"
